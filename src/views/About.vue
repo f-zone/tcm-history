1d94e6919bff4172dbdebfe2226fe295d2db45c7
Vincent Wen <vincentwen@shijiebang.net>
2020-04-11 16:10:19 +0800

    add about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>History of TCM</h1>
+    <p>History of traditional Chinese medicine</p>
+    <p>By 郑阳阳</p>
+    <p>北京中医药大学</p>
+    <p>2020-04-11</p>
   </div>
 </template>
